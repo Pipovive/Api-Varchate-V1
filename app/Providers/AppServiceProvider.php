421bc755ee365c3f651f068bf9ffcbd615c45cf7
Juan Jose Chavez <juanjosechavezgr03@gmail.com>
2026-03-16 08:08:02 -0500

    add services provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,20 +8,17 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Http\Request;
+use Illuminate\Mail\MailManager;
+use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
+use Symfony\Component\Mailer\Transport\Dsn;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         ResetPassword::createUrlUsing(function ($user, string $token) {
@@ -30,9 +27,18 @@ class AppServiceProvider extends ServiceProvider
                 . '&email=' . urlencode($user->email);
         });
 
-
         RateLimiter::for('password-reset', function (Request $request) {
             return Limit::perMinutes(10, 3)->by($request->ip());
+        });
+
+        $this->app->resolving(MailManager::class, function (MailManager $manager) {
+            $manager->extend('brevo', function () {
+                $factory = new BrevoTransportFactory();
+                $dsn = Dsn::fromString(
+                    'brevo+api://' . config('services.brevo.key') . '@default'
+                );
+                return $factory->create($dsn);
+            });
         });
     }
 }
